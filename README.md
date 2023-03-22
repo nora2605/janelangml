cddacc36b192638e65c8949760df85303be3666f
Nora J.F <nora.ja2605@gmail.com>
2023-03-22 20:18:23 +0100

    Initial Site

--- README.md
+++ README.md
@@ -1,34 +1,7 @@
-# Nuxt.js Example
+# Janelang.ml Homepage
 
-Deploy your [Nuxt.js](https://nuxtjs.org) project to Vercel with zero configuration.
+This is the homepage for the Jane language
+Coming to cinemas in 2050
+or not
 
-[![Deploy with Vercel](https://vercel.com/button)](https://vercel.com/new/clone?repository-url=https://github.com/vercel/vercel/tree/main/examples/nuxtjs&template=nuxtjs)
-
-_Live Example: https://nuxtjs-template.vercel.app_
-
-## Setup
-
-Install the dependencies:
-
-```bash
-yarn install
-```
-
-## Development
-
-Start the development server on http://localhost:3000:
-
-```bash
-yarn dev
-```
-
-## Deploy Your Own
-
-Build the application for production:
-
-```bash
-yarn build
-```
-
-
-
+goodbye
--- app.vue
+++ app.vue
@@ -1,5 +1,84 @@
 <template>
-  <div>
-    <NuxtWelcome />
+  <div :class="['container', { 'dark': isDarkMode }]">
+    <h1 :class="['title', { 'dark': isDarkMode }]">Sorry!</h1>
+    <p :class="['subtitle', { 'dark': isDarkMode }]">There's no content available for janelang.ml at the moment. Please check back later.</p>
+    <a href="https://github.com/nora2605/janelangml" target="_blank" rel="noopener noreferrer" :class="['button', { 'dark': isDarkMode }]">What is this?</a>
   </div>
 </template>
+
+<script>
+export default {
+  name: 'App',
+  computed: {
+    isDarkMode() {
+      return true;
+    }
+  }
+}
+</script>
+
+<style>
+html, body {
+  margin: 0px;
+}
+
+.container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  width: 100vw;
+  background-color: #f5f5f5;
+}
+
+.container.dark {
+  background-color: #212121;
+}
+
+.title {
+  font-size: 4rem;
+  color: #212121;
+  margin-bottom: 2rem;
+  text-align: center;
+}
+
+.title.dark {
+  color: #00bcd4;
+}
+
+.subtitle {
+  font-size: 1.5rem;
+  color: #757575;
+  margin-bottom: 2rem;
+  text-align: center;
+}
+
+.subtitle.dark {
+  color: #b2fef7;
+}
+
+.button {
+  padding: 0.75rem 1.5rem;
+  background-color: #00bcd4;
+  color: #fff;
+  border-radius: 0.25rem;
+  text-decoration: none;
+  transition: all 0.3s ease;
+}
+
+.button:hover {
+  background-color: #b2fef7;
+  color: #212121;
+}
+
+.button.dark {
+  background-color: #b2fef7;
+  color: #212121;
+}
+
+.button.dark:hover {
+  background-color: #00bcd4;
+  color: #fff;
+}
+</style>
