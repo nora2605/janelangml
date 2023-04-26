657682f28ac39b2b744044d96c5100e6e7845e53
Nora J.F <nora.ja2605@gmail.com>
2023-04-26 16:43:12 +0200

    add tailwind

--- pages/index.vue
+++ pages/index.vue
@@ -7,3 +7,71 @@
       :class="['button', 'dark']">What is this?</a>
   </div>
 </template>
+
+<style>
+html,
+body {
+  margin: 0px;
+}
+
+.container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  width: 100vw;
+  margin: 0px;
+  background-color: #f5f5f5;
+}
+
+.container.dark {
+  background-color: #212121;
+}
+
+.title {
+  font-size: 4rem;
+  color: #212121;
+  margin-bottom: 2rem;
+  text-align: center;
+}
+
+.title.dark {
+  color: #00bcd4;
+}
+
+.subtitle {
+  font-size: 1.5rem;
+  color: #757575;
+  margin-bottom: 2rem;
+  text-align: center;
+}
+
+.subtitle.dark {
+  color: #b2fef7;
+}
+
+.button {
+  padding: 0.75rem 1.5rem;
+  background-color: #00bcd4;
+  color: #fff;
+  border-radius: 0.25rem;
+  text-decoration: none;
+  transition: all 0.3s ease;
+}
+
+.button:hover {
+  background-color: #b2fef7;
+  color: #212121;
+}
+
+.button.dark {
+  background-color: #b2fef7;
+  color: #212121;
+}
+
+.button.dark:hover {
+  background-color: #00bcd4;
+  color: #fff;
+}
+</style>
