b0d1cb63519ffcd58ed0b5fb878075b1fdbe17e7
Nora J.F <nora.ja2605@gmail.com>
2023-05-09 17:19:58 +0200

    switched node version and now everything works yay

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,10 @@
 <template>
-<div :class="['container', 'dark']">
-    <h1 :class="['title', 'dark']">Sorry!</h1>
-    <p :class="['subtitle', 'dark']">There's no content available for janelang.ml at the moment. Please
+<div class="flex flex-col bg-gray-900 min-h-screen items-center justify-center">
+    <h1 class="title">Sorry!</h1>
+    <p class="subtitle">There's no content available for janelang.ml at the moment. Please
       check back later.</p>
     <a href="https://github.com/nora2605/janelangml" target="_blank" rel="noopener noreferrer"
-      :class="['button', 'dark']">What is this?</a>
+      class="transition text-gray-900 bg-teal-300 hover:text-gray-700 hover:bg-teal-400 button">What is this?</a>
   </div>
 </template>
 
@@ -14,64 +14,23 @@ body {
   margin: 0px;
 }
 
-.container {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  height: 100vh;
-  width: 100vw;
-  margin: 0px;
-  background-color: #f5f5f5;
-}
-
-.container.dark {
-  background-color: #212121;
-}
 
 .title {
   font-size: 4rem;
-  color: #212121;
+  color: #00bcd4;
   margin-bottom: 2rem;
   text-align: center;
-}
-
-.title.dark {
-  color: #00bcd4;
 }
 
 .subtitle {
   font-size: 1.5rem;
-  color: #757575;
+  color: #b2fef7;
   margin-bottom: 2rem;
   text-align: center;
 }
 
-.subtitle.dark {
-  color: #b2fef7;
-}
-
 .button {
-  padding: 0.75rem 1.5rem;
-  background-color: #00bcd4;
-  color: #fff;
-  border-radius: 0.25rem;
-  text-decoration: none;
-  transition: all 0.3s ease;
-}
-
-.button:hover {
-  background-color: #b2fef7;
-  color: #212121;
-}
-
-.button.dark {
-  background-color: #b2fef7;
-  color: #212121;
-}
-
-.button.dark:hover {
-  background-color: #00bcd4;
-  color: #fff;
+  padding: 12px 24px;
+  border-radius: 8px;
 }
 </style>
---
+++ pages/john.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="flex flex-col bg-gray-900 min-h-screen items-center justify-center">
+      <h1 class="title">John</h1>
+      <p class="subtitle">John</p>
+      <a href="https://github.com/nora2605/jane/blob/main/john.md" target="_blank" rel="noopener noreferrer"
+        class="transition text-gray-900 bg-teal-300 hover:text-gray-700 hover:bg-teal-400 button">John</a>
+    </div>
+  </template>
+  
+  <style>
+  html,
+  body {
+    margin: 0px;
+  }
+  
+  
+  .title {
+    font-size: 4rem;
+    color: #00bcd4;
+    margin-bottom: 2rem;
+    text-align: center;
+  }
+  
+  .subtitle {
+    font-size: 1.5rem;
+    color: #b2fef7;
+    margin-bottom: 2rem;
+    text-align: center;
+  }
+  
+  .button {
+    padding: 12px 24px;
+    border-radius: 8px;
+  }
+  </style>
